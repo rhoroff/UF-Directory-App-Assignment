daef8cf8a0e0bf402934e2106f9b0a9f39e5af1e
Ryan Horoff <rhoroff@gmail.com>
2018-09-10 11:08:19 -0400

    Assignment done without CSS Methods of listingController.js changed to allow for individual editing of entries in the listingFactory array

--- listingController.js
+++ listingController.js
@@ -7,8 +7,32 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push(
+        {
+          code : $scope.newBuilding.code, 
+          name: $scope.newBuilding.name,
+          coordinates: {
+            latitude: $scope.newBuilding.latitude,
+            longitude: $scope.newBuilding.longitude
+          },
+          address: $scope.newBuilding.address
+        }
+      )
+      listing.sort;
+    };
+    $scope.deleteListing = function(listing) {
+      var index = $scope.listings.indexOf(listing);
+      $scope.listings.splice(index,1);//Delete just deletes content, spice removes array elements!
+    };
+    $scope.showDetails = function(listing) {
+      var index = $scope.listings.indexOf(listing);
+      $scope.detailedInfo = Listings[index];
+      $scope.detailedInfo.name = detailedInfo.name;;
+      $scope.detailedInfo.code = detailedInfo.code;
+      $scope.detailedInfo.latitude = detailedInfo.coordinates.latitude;
+      $scope.detailedInfo.longitude = detailedInfo.coordinates.longitude;
+      $scope.detailedInfo.address = detailedInfo.address;
+    };
   }
 ]);
